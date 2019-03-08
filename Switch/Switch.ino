cf42c76f5b9449009ccb37b3eeda3854dfab1ef8
赵建伟 <zjwlinux@163.com>
2019-03-08 12:58:33 +0800

    Arduino 电子开关和报销清单

--- Switch/Switch.ino
+++ Switch/Switch.ino
@@ -1,5 +1,5 @@
-byte command[3];
-byte response[4];
+byte command[3] = {0};
+byte response[4] = {0};
 
 //输入命令
 byte command0[3] = {0x53, 0xFE, 0x45};
@@ -32,17 +32,14 @@ void setup() {
 
   while(!Serial){
   }
-
-  //初始化
-  memset(command, 0, sizeof(command));
-  memset(response, 0, sizeof(response));
 }
 
 void loop() {
 
   delay(50);    // 这个时间太小会出现无法预测的问题
   memset(command, 0, sizeof(command));
-  
+
+  //当接收到的数据字节数不等于3的时候，表明当前命令无效，清空接收数据并返回
   if (Serial.available() != 3){
     while(Serial.available() > 0){
       Serial.read();
@@ -50,64 +47,50 @@ void loop() {
     return;
   }
 
+  //读取命令到3个成员的字节数组
   byte count = 0;
   while(Serial.available() > 0){
     command[count++] = Serial.read();        
   }
-  
-  if (arraycmp(command, command0) == 0){      
-    digitalWrite(2, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(7, LOW);
+
+  //确认命令是否有效，无效则不做处理，有效则设置对应的开关并返回响应信息
+  if (arraycmp(command, command0) == 0){    
+    setSwitch(LOW,LOW,LOW);  
     zresponse(response0);
   }
 
-  if (arraycmp(command, command1) == 0){      
-    digitalWrite(2, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(7, LOW);    
+  if (arraycmp(command, command1) == 0){  
+    setSwitch(HIGH,LOW,LOW);        
     zresponse(response1);
   }
 
-  if (arraycmp(command, command2) == 0){      
-    digitalWrite(2, LOW);
-    digitalWrite(4, HIGH);
-    digitalWrite(7, LOW);    
+  if (arraycmp(command, command2) == 0){    
+    setSwitch(LOW,HIGH,LOW);     
     zresponse(response2);
   }
 
   if (arraycmp(command, command3) == 0){      
-    digitalWrite(2, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(7, LOW);
+    setSwitch(HIGH,HIGH,LOW); 
     zresponse(response3);
   }
 
   if (arraycmp(command, command4) == 0){      
-    digitalWrite(2, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(7, HIGH);
+    setSwitch(LOW,LOW,HIGH); 
     zresponse(response4);
   }
 
-  if (arraycmp(command, command5) == 0){      
-    digitalWrite(2, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(7, HIGH);
+  if (arraycmp(command, command5) == 0){   
+    setSwitch(HIGH,LOW,HIGH);    
     zresponse(response5);
   }
 
-  if (arraycmp(command, command6) == 0){      
-    digitalWrite(2, LOW);
-    digitalWrite(4, HIGH);
-    digitalWrite(7, HIGH);
+  if (arraycmp(command, command6) == 0){   
+    setSwitch(LOW,HIGH,HIGH);    
     zresponse(response6);
   }
 
-  if (arraycmp(command, command7) == 0){      
-    digitalWrite(2, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(7, HIGH);
+  if (arraycmp(command, command7) == 0){  
+    setSwitch(HIGH,HIGH,HIGH);     
     zresponse(response7);
   }
 }
@@ -126,4 +109,10 @@ void zresponse(byte *data){
   Serial.write(data[1]);
   Serial.write(data[2]);
   Serial.write(data[3]);
+}
+
+void setSwitch(byte value_a, byte value_b, byte value_c){
+  digitalWrite(2, value_a);
+  digitalWrite(4, value_b);
+  digitalWrite(7, value_c);  
 }
